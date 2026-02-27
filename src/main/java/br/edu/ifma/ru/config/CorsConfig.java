6057754eb45805bda039d08d61ac46f92cd59253
Wesley Eduardo <wesleyeduardo.dev@gmail.com>
2026-02-27 16:00:03 -0300

    Update CorsConfig.java

--- src/main/java/br/edu/ifma/ru/config/CorsConfig.java
+++ src/main/java/br/edu/ifma/ru/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173","https://ru-ifma-frontend.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
